018a4af53fe72f624b7cd667e1a6ab817439ebef
Habeeb Abdulrasaq <halbindeadge@gmail.com>
2023-12-17 14:37:38 +0100

    Queries that provide answers to the problem statement 1

--- Problem_1_Query.sql
+++ Problem_1_Query.sql
@@ -46,25 +46,71 @@ CREATE TABLE loan_payment (
     FOREIGN KEY (loan_id) REFERENCES loan_table(Loan_id)
 );
 
-select * from loan_payment;
-
 -- Load the data into the tables created
 -- Load data into borrower table as downloaded from google drive
-COPY borrower_table FROM 'C:\Users\LENOVO\Desktop\Autochek\Borrower_table.csv' DELIMITER ',' CSV HEADER;
 
--- Load data into loan table table as downloaded from google drive
-COPY loan_table FROM '/path/to/your/loan_table.csv' DELIMITER ',' CSV HEADER;
+COPY public.borrower_table (borrower_id, state, city, zip_code, borrower_credit_score) 
+FROM 'C:/Users/LENOVO/Desktop/Autochek/BORROWER_TABLE.CSV' 
+DELIMITER ',' 
+CSV HEADER 
+QUOTE '"';
 
--- Load data into payment schedule as downloaded from google drive
-COPY payment_schedule FROM 'C:\Users\LENOVO\Desktop\Autochek\Payment_Schedule.csv' DELIMITER ',' CSV HEADER;
+COPY public.loan_table (loan_id, borrower_id, date_of_release, term, interestrate, loanamount, downpayment, payment_frequency, maturity_date) 
+FROM 'C:/Users/LENOVO/Desktop/Autochek/LOAN_TABLE.CSV' 
+DELIMITER ',' 
+CSV HEADER 
+QUOTE '"';
 
--- Load data into loan payment as downloaded from google drive
-COPY loan_payment FROM 'C:\Users\LENOVO\Desktop\Autochek\Loan_payment.csv' DELIMITER ',' CSV HEADER;
+COPY public.payment_schedule (schedule_id, loan_id, expected_payment_date, expected_payment_amount) 
+FROM 'C:/Users/LENOVO/Desktop/Autochek/PAYMENT_SCHEDULE.CSV' 
+DELIMITER ',' 
+CSV HEADER 
+QUOTE '"';
 
+COPY public.loan_payment (payment_id, loan_id, amount_paid, date_paid) 
+FROM 'C:/Users/LENOVO/Desktop/Autochek/LOAN_PAYMENT.CSV' 
+DELIMITER ',' 
+CSV HEADER 
+QUOTE '"';
 
-COPY borrower_table (Borrower_id, State, City, zip_code) FROM 'C:\Users\LENOVO\Desktop\Autochek\Borrower_table.csv' WITH (FORMAT csv, HEADER);
+/*
+Calculate PAR Days - Par Days means the number of days the loan was not paid in full. 
+E.g., If the loan repayment was due on the 10th Feb 2022 and payment was made on the 15th Feb 2022 
+the par days would be 5 days*/
 
+-- Write out queries that profer solutions to the problem statement 1
 
-select *
-from borrower_table;
-
+SELECT
+  b.Borrower_id,
+  b.State,
+  b.City,
+  b.zip_code,
+  l.Loan_id,
+  l.Date_of_release,
+  l.Term,
+  l.InterestRate,
+  l.LoanAmount,
+  l.Downpayment,
+  l.Payment_frequency,
+  l.Maturity_date,
+  ps.Expected_payment_date,
+  ps.Expected_payment_amount,
+  lp.Date_paid,
+  lp.Amount_paid,
+  (lp.Date_paid - ps.Expected_payment_date) AS PAR_Days,
+  SUM(ps.Expected_payment_amount) OVER (PARTITION BY l.Loan_id ORDER BY ps.Expected_payment_date RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Cumulative_Amount_Due,
+  SUM(lp.Amount_paid) OVER (PARTITION BY l.Loan_id ORDER BY lp.Date_paid RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS Cumulative_Amount_Paid,
+  CASE
+    WHEN lp.Date_paid > ps.Expected_payment_date THEN SUM(ps.Expected_payment_amount) OVER (PARTITION BY l.Loan_id ORDER BY ps.Expected_payment_date RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) - SUM(lp.Amount_paid) OVER (PARTITION BY l.Loan_id ORDER BY lp.Date_paid RANGE BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW)
+    ELSE 0
+  END AS Amount_at_Risk
+FROM
+  borrower_table b
+JOIN
+  loan_table l ON b.Borrower_id = l.Borrower_id
+LEFT JOIN
+  payment_schedule ps ON l.Loan_id = ps.loan_id
+LEFT JOIN
+  loan_payment lp ON l.Loan_id = lp.loan_id
+ORDER BY
+  b.Borrower_id, l.Loan_id, ps.Expected_payment_date;
